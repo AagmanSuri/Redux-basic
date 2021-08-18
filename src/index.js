b15a5a8e4fdbf588236dd73de3ac47e07f4c9bef
Aagman Suri <aagmansuri96@gmail.com>
2021-08-19 02:08:04 +0530

    Action

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+
+//Store ==> GLOBALIZED STATE
+
+//Action INCREMENT   (Describes what you whant to do )
+
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+//Reducer
+
+//Dispatch
 
 ReactDOM.render(
   <React.StrictMode>
