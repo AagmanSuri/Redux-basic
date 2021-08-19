4460833e1ac4135f258df21412e5660179dd44a3
Aagman Suri <aagmansuri96@gmail.com>
2021-08-19 13:31:28 +0530

    useDispatch,actions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 
 // to access the piece of state :
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { increment } from "./actions";
+import { decrement, login } from "./actions";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Counter : {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(login())}>Login</button>
       {isLogged && <h1>Confidential data </h1>}
     </div>
   );
