fc9fc9f19a3fd52d195b02f70904b7d80b4b2037
Aagman Suri <aagmansuri96@gmail.com>
2021-08-19 11:46:56 +0530

    REDUX

--- src/index.js
+++ src/index.js
@@ -2,45 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+//import { createStore } from "redux";
 
-//Store ==> GLOBALIZED STATE
+// //Store ==> GLOBALIZED STATE
 
-//Action INCREMENT   (Describes what you whant to do )
+// //Action INCREMENT   (Describes what you whant to do )
 
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
 
-//Reducer
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-};
+// //Reducer
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
 
-let store = createStore(counter);
+// let store = createStore(counter);
 
-//Display it in the console
+// //Display it in the console
 
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 
-//Dispatch
+// //Dispatch
 
-store.dispatch(increment());
-store.dispatch(decrement());
+// store.dispatch(increment());
+// store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
